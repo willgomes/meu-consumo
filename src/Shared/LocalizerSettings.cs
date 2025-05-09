59a5fb336f926466eea2a946a637d61ee9c176e6
Wilson Neto <will_lovinf@live.com>
2025-05-09 17:03:33 -0300

    feat: Add Spanish localization support with resource file and update culture settings

--- src/Shared/LocalizerSettings.cs
+++ src/Shared/LocalizerSettings.cs
@@ -4,9 +4,9 @@ public static class LocalizerSettings
 {
     public const string NeutralCulture = "pt-BR";
 
-    public static readonly string[] SupportedCultures = [NeutralCulture, "en-US"];
+    public static readonly string[] SupportedCultures = [NeutralCulture, "en-US", "es-MX"];
 
-    public static readonly (string, string)[] SupportedCulturesWithName = [("Português Brasileiro", NeutralCulture), ("English", "en-US")];
+    public static readonly (string, string)[] SupportedCulturesWithName = [("Português Brasileiro", NeutralCulture), ("English", "en-US"), ("Spanish", "es-MX")];
 
     public const string CULTURE_KEY = "preferred-culture";
 
