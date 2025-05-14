2ce271621bb49582a2b9c00da31886035e7b3f38
Wilson Neto <will_lovinf@live.com>
2025-05-14 16:19:15 -0300

    feat: Add product expiration and update details in Product component

--- src/Pages/Product.razor.cs
+++ src/Pages/Product.razor.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Components;
 using Microsoft.Extensions.Localization;
 
 using Models;
+
 using MudBlazor;
 
 using Resources;
@@ -15,6 +16,7 @@ namespace track_items.Pages
         [Inject] ProductService? ProductService { get; set; }
         [Inject] ISnackbar? Snackbar { get; set; }
         [Inject] IStringLocalizer<Resource> Localizer { get; set; }
+        [Inject] NavigationManager NavigationManager { get; set; }
 
         private IEnumerable<ProductModel>? _products = [];
 
@@ -32,6 +34,8 @@ namespace track_items.Pages
 
             await LoadProductsAsync();
         }
+
+        private void EditProduct(Guid id) => NavigationManager.NavigateTo($"/product-form/{id}");
 
         private async Task LoadProductsAsync() => _products = await ProductService!.GetProductsAsync();
 
